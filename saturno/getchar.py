b37cd01e1e25855f41b3116dfdd2a7b6be05c051
Moris Doratiotto <moris.doratiotto@gmail.com>
2021-03-01 14:08:35 +0100

    Fix: remove useless import and code

--- saturno/getchar.py
+++ saturno/getchar.py
@@ -1,12 +1,11 @@
 class _Getch:
     def __init__(self):
         try:
+            print("WIN")
             self.impl = _GetchWindows()
         except ImportError:
-            try:
-                self.impl = _GetchUnix()
-            except ImportError:
-                self.impl = _GetchMacCarbon()
+            print("UNIX")
+            self.impl = _GetchUnix()
 
     def __call__(self):
         return self.impl()
@@ -14,7 +13,7 @@ class _Getch:
 
 class _GetchUnix:
     def __init__(self):
-        import tty, sys, termios  # import termios now or else you'll get the Unix version on the Mac
+        import tty, sys, termios
 
     def __call__(self):
         import sys, tty, termios
@@ -37,29 +36,3 @@ class _GetchWindows:
         import msvcrt
 
         return msvcrt.getch()
-
-
-class _GetchMacCarbon:
-    def __init__(self):
-        import Carbon
-
-    def __call__(self):
-        import Carbon
-
-        if Carbon.Evt.EventAvail(0x0008)[0] == 0:  # 0x0008 is the keyDownMask
-            return ""
-        else:
-            (what, msg, when, where, mod) = Carbon.Evt.GetNextEvent(0x0008)[1]
-            return chr(msg & 0x000000FF)
-
-
-if __name__ == "__main__":  # a little test
-    print("Move: ", end="\r")
-    inkey = _Getch()
-    import sys
-
-    while True:
-        k = inkey()
-        print("Move: ", k, end="\r")
-        if k == "q":
-            break

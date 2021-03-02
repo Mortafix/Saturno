6e4817e65092898fdcf200fa0bfad67e7fcaf501
Moris Doratiotto <moris.doratiotto@gmail.com>
2021-03-02 18:09:26 +0100

    Update: version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="saturno",
-    version="1.3.0",
+    version="1.3.1",
     author="Moris Doratiotto",
     author_email="moris.doratiotto@gmail.com",
     description="A python module to download anime from Anime Saturn",
